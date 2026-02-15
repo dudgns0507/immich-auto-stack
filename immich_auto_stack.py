20e30c4b7a9d4e2d5e5d3849a0f1bd3d25f89e05
cropo <hoon2585@gmail.com>
2026-02-16 07:35:37 +0900

    fix: deprecated api patch

--- immich_auto_stack.py
+++ immich_auto_stack.py
@@ -6,6 +6,7 @@ import json
 import os
 import re
 import time
+from dotenv import load_dotenv
 
 from str2bool import str2bool
 from requests import Session
@@ -14,30 +15,32 @@ from urllib3.util.retry import Retry
 from urllib.parse import urlparse
 
 logging.basicConfig(
-  stream=sys.stdout, 
-  level=logging.INFO, 
-  format='%(asctime)s - %(levelname)s - %(message)s'
+    stream=sys.stdout,
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(message)s'
 )
 logger = logging.getLogger(__name__)
 
 criteria_default = [
-  {
-    "key": "originalFileName",
-    "split": {
-      "key": ".",
-      "index": 0
+    {
+        "key": "originalFileName",
+        "split": {
+            "key": ".",
+            "index": 0
+        }
+    },
+    {
+        "key": "localDateTime"
     }
-  },
-  {
-    "key": "localDateTime"
-  }
 ]
+
 
 def get_criteria_config():
     criteria_override = os.environ.get("CRITERIA")
     if criteria_override:
         return json.loads(criteria_override)
     return criteria_default
+
 
 def apply_criteria(x: dict) -> list:
     """
@@ -70,182 +73,228 @@ def apply_criteria(x: dict) -> list:
             regex_index = item["regex"].get("index", 1)
             match = re.match(regex_key, value)
             if match:
-              value = match.group(regex_index)
+                value = match.group(regex_index)
             elif not str2bool(os.environ.get("SKIP_MATCH_MISS")):
-              raise Exception(f"Match not found for value: {value}, regex: {regex_key}")
+                raise Exception(f"Match not found for value: {value}, regex: {regex_key}")
             else:
-              return []
+                return []
         criteria_list.append(value)
     return criteria_list
 
+
 def parent_criteria(x):
-  parent_ext = ['.jpg', '.jpeg', '.png']
+    parent_ext = ['.jpg', '.jpeg', '.png']
 
-  parent_promote = list(filter(None, os.environ.get("PARENT_PROMOTE", "").split(",")))
-  parent_promote_baseline = 0
+    parent_promote = list(filter(None, os.environ.get("PARENT_PROMOTE", "").split(",")))
+    parent_promote_baseline = 0
 
-  lower_filename = x["originalFileName"].lower()
+    lower_filename = x["originalFileName"].lower()
 
-  if any(lower_filename.endswith(ext) for ext in parent_ext):
-    parent_promote_baseline -= 100
+    if any(lower_filename.endswith(ext) for ext in parent_ext):
+        parent_promote_baseline -= 100
 
-  for key in parent_promote:
-    if key.lower() in lower_filename:
-      logger.info("promoting " + x["originalFileName"] + f" for key {key}")
-      parent_promote_baseline -= 1
+    for key in parent_promote:
+        if key.lower() in lower_filename:
+            logger.info("promoting " + x["originalFileName"] + f" for key {key}")
+            parent_promote_baseline -= 1
 
-  return [parent_promote_baseline, x["originalFileName"]]
+    return [parent_promote_baseline, x["originalFileName"]]
 
 
 class Immich():
-  def __init__(self, url: str, key: str):
-    self.api_url = f'{urlparse(url).scheme}://{urlparse(url).netloc}/api'
-    self.headers = {
-      'x-api-key': key,
-      'Accept': 'application/json'
-    }
-    self.assets = list()
-  
-  def fetchAssets(self, size: int = 1000) -> list:
-    payload = {
-      'size' : size,
-      'page' : 1,
-      #'withExif': True,
-      'withStacked': True
-    }
-    assets_total = list()
+    def __init__(self, url: str, key: str):
+        self.api_url = f'{urlparse(url).scheme}://{urlparse(url).netloc}/api'
+        self.headers = {
+            'x-api-key': key,
+            'Accept': 'application/json'
+        }
+        self.assets = list()
+        self.stacks = list()
 
-    logger.info(f'⬇️  Fetching assets: ')
-    logger.info(f'   Page size: {size}')
+    def fetchStacks(self):
+        stack_assets_total = list()
 
-    session = Session()
-    retry = Retry(connect=3, backoff_factor=0.5)
-    adapter = HTTPAdapter(max_retries=retry)
-    session.mount('http://', adapter)
-    session.mount('https://', adapter)
+        logger.info(f'⬇️  Fetching stacks: ')
 
-    while payload["page"] != None:
-      response = session.post(f"{self.api_url}/search/metadata", headers=self.headers, json=payload)
+        session = Session()
+        retry = Retry(connect=3, backoff_factor=0.5)
+        adapter = HTTPAdapter(max_retries=retry)
+        session.mount('http://', adapter)
+        session.mount('https://', adapter)
 
-      if not response.ok:
-        logger.error('   Error:', response.status_code, response.text)
+        response = session.get(f"{self.api_url}/stacks", headers=self.headers)
 
-      response_data = response.json()
-      assets_total = assets_total + response_data['assets']['items']
-      payload["page"] = response_data['assets']['nextPage']
-    
-    self.assets = assets_total
-    
-    logger.info(f'   Pages: {payload["page"]}')   
-    logger.info(f'   Assets: {len(self.assets)}')
-    
-    return self.assets
+        if not response.ok:
+            logger.error('   Error:', response.status_code, response.text)
 
-  def modifyAssets(self, payload: dict) -> None:
-    session = Session()
-    retry = Retry(connect=3, backoff_factor=0.5)
-    adapter = HTTPAdapter(max_retries=retry)
-    session.mount('http://', adapter)
-    session.mount('https://', adapter)
+        response_data = response.json()
 
-    response = session.put(f"{self.api_url}/assets", headers=self.headers, json=payload)
+        for stack in response_data:
+            stack_assets_total = stack_assets_total + [x["id"] for x in stack['assets']]
 
-    if response.ok:
-      logger.info("  🟢 Success!")
-    else:
-      logger.error(f"  🔴 Error! {response.status_code} {response.text}") 
+        self.stacks = stack_assets_total
+
+        logger.info(f'   Stacks: {len(response_data)}')
+
+        return self.stacks
+
+    def fetchAssets(self, size: int = 1000) -> list:
+        payload = {
+            'size': size,
+            'page': 1,
+        }
+        assets_total = list()
+
+        logger.info(f'⬇️  Fetching assets: ')
+        logger.info(f'   Page size: {size}')
+
+        session = Session()
+        retry = Retry(connect=3, backoff_factor=0.5)
+        adapter = HTTPAdapter(max_retries=retry)
+        session.mount('http://', adapter)
+        session.mount('https://', adapter)
+
+        while payload["page"] is not None:
+            response = session.post(f"{self.api_url}/search/metadata", headers=self.headers, json=payload)
+
+            if not response.ok:
+                logger.error('   Error:', response.status_code, response.text)
+
+            response_data = response.json()
+            assets_total = assets_total + response_data['assets']['items']
+            payload["page"] = response_data['assets']['nextPage']
+
+        self.assets = assets_total
+
+        logger.info(f'   Pages: {payload["page"]}')
+        logger.info(f'   Assets: {len(self.assets)}')
+
+        return self.assets
+
+    def createStack(self, payload: dict) -> None:
+        session = Session()
+        retry = Retry(connect=3, backoff_factor=0.5)
+        adapter = HTTPAdapter(max_retries=retry)
+        session.mount('http://', adapter)
+        session.mount('https://', adapter)
+
+        response = session.post(f"{self.api_url}/stacks", headers=self.headers, json=payload)
+
+        if response.ok:
+            logger.info("  🟢 Success!")
+        else:
+            logger.error(f"  🔴 Error! {response.status_code} {response.text}")
+
+    def modifyAssets(self, payload: dict) -> None:
+        session = Session()
+        retry = Retry(connect=3, backoff_factor=0.5)
+        adapter = HTTPAdapter(max_retries=retry)
+        session.mount('http://', adapter)
+        session.mount('https://', adapter)
+
+        response = session.put(f"{self.api_url}/assets", headers=self.headers, json=payload)
+
+        if response.ok:
+            logger.info("  🟢 Success!")
+        else:
+            logger.error(f"  🔴 Error! {response.status_code} {response.text}")
 
 
 def stackBy(data: list, criteria) -> list:
-  # Optional: remove incompatible file names
-  if str2bool(os.environ.get("SKIP_MATCH_MISS")):
-    data = filter(criteria, data)
+    # Optional: remove incompatible file names
+    if str2bool(os.environ.get("SKIP_MATCH_MISS")):
+        data = filter(criteria, data)
 
-  # Sort by primary and secondary criteria
-  data = sorted(data, key=criteria)
+    # Sort by primary and secondary criteria
+    data = sorted(data, key=criteria)
 
-  # Group by primary and secondary criteria
-  groups = groupby(data, key=criteria)
-  
-  # Extract and process groups into a list of tuples
-  groups = [(key, list(group)) for key, group in groups]
-  
-  # Filter only groups that have more than one item
-  groups = [x for x in groups if len(x[1]) > 1 ] 
+    # Group by primary and secondary criteria
+    groups = groupby(data, key=criteria)
 
-  # Raise error if any groups have an empty key
-  if any((group[0] == [] or None in group[0]) for group in groups):
-      raise Exception(
-          "Some photos do not match the criteria you provided. Consider refining your"
-          "criteria. If the criteria was not intended to match all files, use the"
-          "SKIP_MATCH_MISS environment variable to skip processing of those photos."
-      )
+    # Extract and process groups into a list of tuples
+    groups = [(key, list(group)) for key, group in groups]
 
-  return groups
+    # Filter only groups that have more than one item
+    groups = [x for x in groups if len(x[1]) > 1]
+
+    # Raise error if any groups have an empty key
+    if any((group[0] == [] or None in group[0]) for group in groups):
+        raise Exception(
+            "Some photos do not match the criteria you provided. Consider refining your"
+            "criteria. If the criteria was not intended to match all files, use the"
+            "SKIP_MATCH_MISS environment variable to skip processing of those photos."
+        )
+
+    return groups
+
 
 def stratifyStack(stack: list) -> list:
-  # Ensure the desired parent is first in the list
-  return sorted(stack, key=parent_criteria)
+    # Ensure the desired parent is first in the list
+    return sorted(stack, key=parent_criteria)
 
 
 def main():
+    load_dotenv()
+    api_key = os.environ.get("API_KEY", False)
 
-  api_key = os.environ.get("API_KEY", False)
+    api_url = os.environ.get("API_URL", "http://immich_server:3001/api")
 
-  api_url = os.environ.get("API_URL", "http://immich_server:3001/api")
+    skip_previous = str2bool(os.environ.get("SKIP_PREVIOUS", True))
 
-  skip_previous = str2bool(os.environ.get("SKIP_PREVIOUS", True))
+    dry_run = str2bool(os.environ.get("DRY_RUN", False))
 
-  dry_run = str2bool(os.environ.get("DRY_RUN", False))
+    if not api_key:
+        logger.warn("API key is required")
+        return
 
-  if not api_key:
-    logger.warn("API key is required")
-    return
+    logger.info('============== INITIALIZING ==============')
 
-  logger.info('============== INITIALIZING ==============')
+    if dry_run:
+        logger.info('🔒  Dry run enabled, no changes will be applied')
 
-  if dry_run:
-    logger.info('🔒  Dry run enabled, no changes will be applied')
-  
-  immich = Immich(api_url, api_key)
-  
-  assets = immich.fetchAssets()
+    immich = Immich(api_url, api_key)
 
-  stacks = stackBy(assets, apply_criteria)
+    exist_stacks = immich.fetchStacks()
+    assets = immich.fetchAssets()
 
-  for i, v in enumerate(stacks):
-    key, stack = v
+    temp_stacks = stackBy(assets, apply_criteria)
+    stacks = []
 
-    stack = stratifyStack(stack)
+    for i, v in enumerate(temp_stacks):
+        key, stack = v
+        stack = stratifyStack(stack)
+        skip_previous = False
+        for item in stack:
+            if item["id"] in exist_stacks:
+                skip_previous = True
+                break
 
-    parent_id = stack[0]['id']
-    children_id = []
-    
-    if skip_previous:
-      children_id = [x['id'] for x in stack[1:] if x['stackCount'] == None ]
-      
-      if len(children_id) == 0:
-        logger.info(f'{i}/{len(stacks)} Key: {key} SKIP! No new children!')
-        continue
-      
-    else:
-      children_id = [x['id'] for x in stack[1:]]
+        if not skip_previous:
+            stacks.append((key, stack))
 
-    logger.info(f'{i}/{len(stacks)} Key: {key}')
-    logger.info(f'   Parent name: {stack[0]["originalFileName"]} ID: {parent_id}')
-    
-    for child in stack[1:]:
-      logger.info(f'   Child name:  {child["originalFileName"]} ID: {child["id"]}')
+    for i, v in enumerate(stacks):
+        key, stack = v
 
-    if len(children_id) > 0:
-      payload = {
-        "ids": children_id,
-        "stackParentId": parent_id
-      }
+        stack = stratifyStack(stack)
 
-      if not dry_run:
-        time.sleep(.1)
-        immich.modifyAssets(payload)
+        parent_id = stack[0]['id']
+        children_id = [x['id'] for x in stack[1:]]
+
+        logger.info(f'{i}/{len(stacks)} Key: {key}')
+        logger.info(f'   Parent name: {stack[0]["originalFileName"]} ID: {parent_id}')
+
+        for child in stack[1:]:
+            logger.info(f'   Child name:  {child["originalFileName"]} ID: {child["id"]}')
+
+        if len(children_id) > 0:
+            payload = {
+                "assetIds": [parent_id, child["id"]],
+            }
+
+            if not dry_run:
+                time.sleep(.1)
+                immich.createStack(payload)
+
 
 if __name__ == '__main__':
-  main()
+    main()
